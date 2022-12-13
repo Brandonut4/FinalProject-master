8fdb97b7c5985b7ca9d90e9fccaf0d788f6e6439
Brandon Jacobs <brandon.jacobs345@gmail.com>
2022-12-13 15:23:53 -0700

    more stuff

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from dotenv import load_dotenv
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -80,16 +81,22 @@ WSGI_APPLICATION = 'finalProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'recipes', 
+#     'USER': 'postgres',
+#     'PASSWORD': 'Bj9934245',
+#     'HOST': 'localhost',
+#     'PORT': 5433
+#     }
+# } 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'recipes', 
-    'USER': 'postgres',
-    'PASSWORD': 'Bj9934245',
-    'HOST': 'localhost',
-    'PORT': 5433
-    }
-} 
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 
 # Password validation
